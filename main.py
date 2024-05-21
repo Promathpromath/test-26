cadf41e2e828866b7677224d4cc2c9ed27c744ee
Dmitry_Tikhonov <tdmv1209@gmail.com>
2024-05-21 16:52:03 +0300

    Added main

---
+++ main.py
@@ -0,0 +1,17 @@
+class Hero:
+    def __init__(self, name, health, power):
+        self.name = name
+        self.health = health
+        self.power = power
+    
+    def fight(self, enemy):
+        enemy.health -= self.power
+
+
+
+knight = Hero('Вася', 100, 10)
+print(knight.name)
+
+knight2 = Hero('Петя', 120, 5)
+knight.fight(knight2)
+print(knight2.health)
